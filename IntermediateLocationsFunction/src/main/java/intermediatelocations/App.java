fb47df2c784fd018f43a4955c7286963ecfa387a
kevinsunchen <kschen@mit.edu>
2022-01-17 01:57:12 -0500

    implemented new architecture in template.yaml, restored gradle wrapper

--- IntermediateLocationsFunction/src/main/java/intermediatelocations/App.java
+++ IntermediateLocationsFunction/src/main/java/intermediatelocations/App.java
@@ -1,4 +1,4 @@
-package helloworld;
+package intermediatelocations;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- LocalizationFunction/src/test/java/localizationtask/AppTest.java
+++ LocalizationFunction/src/test/java/localizationtask/AppTest.java
@@ -1,4 +1,4 @@
-package helloworld;
+package localization;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;

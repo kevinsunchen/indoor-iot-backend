94d66e4986ee9c1dfa09e5c14cc1ca279bd0b28e
kevinsunchen <kschen@mit.edu>
2022-01-19 17:38:23 -0500

    converted localization_function to python, added numpy and scipy requirements

---
+++ IntermediateLocationsFunction/src/main/java/intermediatelocations/MeasurementsHandler.java
@@ -0,0 +1,33 @@
+package intermediatelocations;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class MeasurementsHandler implements RequestHandler<DynamodbEvent, String> {
+
+    public String handleRequest(final DynamodbEvent ddbEvent, final Context context) {
+        System.out.println("Hello");
+        for (DynamodbStreamRecord record : ddbEvent.getRecords()) {
+            System.out.println(record.getEventID());
+            System.out.println(record.getEventName());
+            // System.out.println(record.getDynamodb().toString() + "\n");
+            System.out.println(record.getDynamodb().getNewImage() + "\n");
+        }
+        return "";
+    }
+
+
+}
---
+++ localization_function/localization_handler.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import numpy
+import scipy
+
+def handle_request(event, context):
+    print(numpy.abs(-100))
+    return "Hello hello"

343304dc44946b0be81358857d32a76a1bb67f1b
kevinsunchen <kschen@mit.edu>
2022-01-21 13:26:28 -0500

    setting up python boto3 dynamodb client

--- localization_function/localization_handler.py
+++ localization_function/localization_handler.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 import boto3
 import localize_utils
 
+client = boto3.client('dynamodb')
+
+
 def handle_request(event, context):
     for record in event["Records"]:
         print(record)
